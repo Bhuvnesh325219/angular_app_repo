6ba227e738c96f4d2fb08cc3ced3f53459766b6b
Bhuvnesh325219 <bhuvnesh325219@gmail.com>
2023-02-11 19:23:44 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ 
+template: '<h1>Hello {{name}}</h1><a href={{link}}>Visit My React App</a>'
 })
+
 export class AppComponent {
-  title = 'demo_angular_app';
+  title = 'basicAngular';
+  name = 'this is my demo angular app'
+  link = 'https://bhuvnesh325219.github.io/react_demo_app/'
 }
